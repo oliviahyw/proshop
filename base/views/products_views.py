41bffec17450725410d3f5270759b38061a86375
oliviahyw <112413539+oliviahyw@users.noreply.github.com>
2024-05-23 11:01:54 -0400

    updated paginator

--- base/views/products_views.py
+++ base/views/products_views.py
@@ -20,7 +20,7 @@ def getProducts(request):
     products = Product.objects.filter(name__icontains=query)
 
     page = request.query_params.get('page')
-    paginator = Paginator(products, 4)
+    paginator = Paginator(products, 8)
 
     try:
         products = paginator.page(page)
@@ -40,7 +40,7 @@ def getProducts(request):
 
 @api_view(['GET'])
 def getTopProducts(request):
-    products = Product.objects.filter(rating__gte=4).order_by('-rating')[0:5]
+    products = Product.objects.filter(rating__gte=4).order_by('-rating')[0:8]
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
 
